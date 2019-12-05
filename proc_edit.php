f7ab9458a1ba015771e6f8d63d7c2c9e3f39e1be
paulorisucci <54944227+paulorisucci@users.noreply.github.com>
2019-12-04 21:08:04 -0300

    Update proc_edit.php

--- proc_edit.php
+++ proc_edit.php
@@ -2,7 +2,7 @@
 session_start();
 include_once("conexao.php");
 
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING); // recebe as variáveis e filtra-as
 $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 $tipo = filter_input(INPUT_POST, 'tipo', FILTER_SANITIZE_STRING);
 $numero = filter_input(INPUT_POST, 'numero', FILTER_SANITIZE_NUMBER_INT);
@@ -16,17 +16,15 @@ $localidade = filter_input(INPUT_POST, 'localidade', FILTER_SANITIZE_STRING);
 </head>
 <body>
     <?php
-    echo "<a href = 'listar.php'>Lista </a>";
-    echo "<a href = index.php>Registrar</a>";
-    include_once("conexao.php");
-    $conexao =  mysqli_connect("localhost", "root", "", "pokemon");
+  
 
 
 
     $result_pokemons = "UPDATE pokemons SET nome = '$nome', tipo = '$tipo', numero = '$numero', localidade = '$localidade', modificado = NOW() WHERE id = '$id'";
-    $resultado_pokemon = mysqli_query($conn, $result_pokemons);
+    // RESULT POKKEMONS atualiza os dados dentro do banco de dados, para uma tabela com ID específico, com as variáveis recebidas pelo FILTER SANITIZE.
+    $resultado_pokemon = mysqli_query($conn, $result_pokemons); // consulta a tabela
 
-    if(mysqli_affected_rows($conn)) {
+    if(mysqli_affected_rows($conn)) { // se alguma variável foi alterada, imprime a mensagem de sucesso
         $_SESSION['msg'] = "<p style ='color: green'>Pokémon alterado com sucesso!</p>";
         header('location: listar.php');
         }
